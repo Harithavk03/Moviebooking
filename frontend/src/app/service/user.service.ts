57a4031315ea55a6c0a6a5d385af41f9f5d031c2
Harithavk03 <94163693+Harithavk03@users.noreply.github.com>
2022-02-08 15:17:40 +0530

    Adding all the files

---
+++ frontend/src/app/service/user.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+const baseUrl="http://localhost:3500/api/users";
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+}
