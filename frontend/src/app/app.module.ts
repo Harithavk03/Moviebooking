998d500beeb6445cd70f1c24d85e3c8ef187be53
Harithavk03 <94163693+Harithavk03@users.noreply.github.com>
2022-02-08 15:39:57 +0530

    Adding all the files

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoPageComponent } from './no-page/no-page.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { HeaderComponent } from './header_footer/header/header.component';
+import { FooterComponent } from './header_footer/footer/footer.component';
+import { TicketBookingsComponent } from './bookings/ticket-bookings/ticket-bookings.component';
+import { MoviesComponent } from './bookings/movies/movies.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    RegisterComponent,
+    LoginComponent,
+    NoPageComponent,
+    MovieListComponent,
+    HeaderComponent,
+    FooterComponent,
+    TicketBookingsComponent,
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
